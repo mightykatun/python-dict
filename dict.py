bc0df89fd7274f3148c85f3e24be8a80049d8422
mightykatun <91433828+mightykatun@users.noreply.github.com>
2022-05-15 20:29:53 +0200

    Add files via upload

---
+++ dict.py
@@ -0,0 +1,25 @@
+import ast
+print("Python Dictionary")
+print("loading...")
+file = open("dict.txt", "r", encoding = "utf8")
+content = file.readlines()
+strcontent = "".join(map(str, content))
+dictionary = ast.literal_eval(strcontent)
+print("done")
+while True:
+    wrequest = input("definition of ")
+    try:
+        print("")
+        print(dictionary[wrequest])
+    except KeyError:
+        print("word is not in dictionary")
+        continue
+    while True:
+        follow = input("continue y/n ")
+        if follow == "y":
+            break
+        if follow == "n":
+            input("enter to exit")
+            exit()
+        else:
+            print("input exception")
